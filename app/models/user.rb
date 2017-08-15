11255ca503cdeaedaeba40f8cdeaa8ac1fc39f57
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-14 22:47:16 -0700

    Link to facebook auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
       user.name = auth_hash['info']['name']
       user.location = auth_hash['info']['location']
       user.image_url = auth_hash['info']['image']
-      user.url = auth_hash['info']['urls']['Twitter']
+      user.url = auth_hash['info']['urls'][user.provider.capitalize]
       user.save!
       user
     end
