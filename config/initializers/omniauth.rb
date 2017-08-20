70affc73a676362049bfe00a643b2d9b0a44fd31
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-19 22:40:30 -0700

    Fix typo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,4 +6,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
   OmniAuth.config.on_failure = Proc.new do |env|
     SessionsController.action(:auth_failure).call(env)
+  end
 end
